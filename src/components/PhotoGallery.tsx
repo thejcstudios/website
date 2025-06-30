325a58239d805b61320d2c948575b74317f5329e
MacBook Air <Jcstudios.database@gmail.com>
2025-06-30 11:56:46 +0800

    added sitemap

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -11,7 +11,7 @@ function PhotoGallery() {
 
       <div className={`offer2 ${isAboutVisible ? "visible" : ""}`} ref={aboutRef}>
         <p>
-        We provide professional photo and video coverage designed to meet the demands of high-impact events. From corporate functions and brand launches to formal celebrations and conferences, our team delivers polished, results-driven visual content that reflects your brand’s standards and objectives. With a focus on reliability, efficiency, and visual excellence, we turn your key moments into compelling content that drives engagement and leaves a lasting impression.
+        We provide professional photo and video coverage designed to meet the demands of high-impact events. From corporate functions and brand launches to formal celebrations and conferences, our team delivers polished results that reflects your brand’s standards and objectives. With a focus on reliability, efficiency, and excellence, we turn your key moments into compelling visual that drives engagement and leaves a lasting impression.
         </p>
       </div>
 
